63bed6047fd3d2235e40ce98ffd02d59a94041f6
Rein <151997117+Rein-AI-Lab@users.noreply.github.com>
2024-01-26 00:25:57 +0900

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,29 @@
+import pygame, sys
+from grid import Grid
+from blocks import *
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))  # width, height
+pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock() # frame control
+
+game_grid = Grid()
+
+block = TBlock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Drawing
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+    block.draw(screen)
+    
+    pygame.display.update()
+    clock.tick(60)
+

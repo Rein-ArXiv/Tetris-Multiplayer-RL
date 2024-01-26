848a84e7c8cd05f40cd620cefc18ca5bbd706f9f
Rein <151997117+Rein-AI-Lab@users.noreply.github.com>
2024-01-26 10:49:35 +0900

    Add files via upload

--- python/main.py
+++ python/main.py
@@ -1,29 +1,69 @@
 import pygame, sys
-from grid import Grid
-from blocks import *
+from game import Game
+from colors import Colors
 
 pygame.init()
-dark_blue = (44, 44, 127)
 
-screen = pygame.display.set_mode((300, 600))  # width, height
+title_font = pygame.font.Font(None, 40)
+score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER", True, Colors.white)
+
+
+score_rect = pygame.Rect(320, 55, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
+
+screen = pygame.display.set_mode((500, 620))  # width, height
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock() # frame control
 
-game_grid = Grid()
+game = Game()
 
-block = TBlock()
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 500)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+
+            if (event.key == pygame.K_LEFT or event.key == ord('a')) and game.game_over == False:      # LEFT arrow key
+                game.move_left()
+            if (event.key == pygame.K_RIGHT or event.key == ord('d')) and game.game_over == False:
+                game.move_right()
+            if (event.key == pygame.K_DOWN or event.key == ord('s')) and game.game_over == False:
+                game.move_down()
+            if (event.key == pygame.K_UP or event.key == ord('w')) and game.game_over == False:
+                game.rotate()
+            if event.key == pygame.K_SPACE:
+                game.drop()
+
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+
     #Drawing
-    screen.fill(dark_blue)
-    game_grid.draw(screen)
-    block.draw(screen)
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+
+    screen.fill(Colors.dark_blue)
+    screen.blit(score_surface, (365, 20, 50, 50))
+    screen.blit(next_surface, (375, 180, 50, 50))
     
+    if game.game_over == True:
+        screen.blit(game_over_surface, (320, 450, 50, 50))
+
+    pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery))
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
+
+    game.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
 
